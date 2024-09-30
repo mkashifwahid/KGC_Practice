99283712157bd6c424f8c30608e28b5092cfd0a4
Muhammad Kashif <mkashif.aw@gmail.com>
2024-09-30 20:50:44 +0500

    react portfolio project

---
+++ practice/04_react/tailwind-portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div  className="bg-slate-100 px-3 py-8">
+      <div className="bg-white max-w-4xl mx-auto rounded">
+        PlaceOrder
+        <h1 className="text-rose-500">Placeholder</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, consequuntur? Commodi, accusantium voluptatum incidunt officiis laborum dignissimos? Neque magni fuga voluptas? Aliquid veritatis aspernatur nostrum facilis perferendis laudantium possimus at cum magnam ducimus necessitatibus odit voluptatem illo recusandae eveniet, rerum natus obcaecati iusto quam dolor quae quisquam fuga atque. Incidunt mollitia, ex aut quasi corrupti voluptatibus tempora reiciendis laborum quas porro. Obcaecati amet aspernatur ipsam sunt fugiat, porro reiciendis sequi expedita mollitia consequuntur qui exercitationem nostrum? Accusantium consectetur minima quasi tempore aliquid, sapiente natus illum veniam pariatur magni numquam molestiae voluptatibus temporibus exercitationem reprehenderit iure quos sed quisquam fugit minus veritatis, suscipit blanditiis totam. Ipsa consectetur, numquam enim quam asperiores provident sit fugiat harum tenetur animi corporis omnis eaque, laborum ducimus, architecto necessitatibus dolores? Ducimus ratione doloribus, voluptatum eligendi at, cupiditate nihil atque incidunt dolores quod, assumenda voluptates architecto necessitatibus culpa. Fugit, iusto at inventore in, expedita libero dicta, veritatis aperiam magnam soluta amet nisi! Ipsum excepturi labore quam impedit! Exercitationem facere debitis quam enim quae ipsam officiis magni, veniam beatae et placeat pariatur laudantium minima illo voluptatum ipsum possimus impedit asperiores, quibusdam delectus! Iusto quo magnam exercitationem nulla? Veritatis quas eum iure est facilis assumenda, ab modi delectus doloremque dignissimos enim, exercitationem natus, adipisci ipsa voluptates? Maiores atque, perferendis quam sint odio facere nam maxime error repellat, assumenda quis numquam deserunt, sunt laboriosam. Alias, animi voluptatem? Blanditiis consectetur assumenda excepturi explicabo molestias quae quis perferendis voluptas quidem, placeat quibusdam vitae. Doloremque nostrum similique quo itaque quibusdam eos quae, earum quasi officiis sit voluptate quos delectus facilis dolor ab ullam debitis. Nesciunt debitis aspernatur unde autem fugit vitae vero tempora! Facere obcaecati maiores adipisci quod, veniam voluptate veritatis blanditiis voluptates modi repellendus aliquid? Accusantium est reiciendis explicabo dicta repellendus iusto mollitia recusandae! Debitis, natus. Nemo quo soluta optio labore consequuntur.</p>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
